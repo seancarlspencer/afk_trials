d4955419c3f7ffa5783260aa314a7b8624af18f3
seancarlspencer <47134137+seancarlspencer@users.noreply.github.com>
2020-10-31 12:07:11 -0400

    Deploy

---
+++ build/precache-manifest.95d8adb6930e3c545e71c01d7ac93118.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32148ae1c99029b425dc73ee30949e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b60bff2f4a23814a52",
+    "url": "/static/css/main.16ecfdcf.chunk.css"
+  },
+  {
+    "revision": "1c8acc8cd215e3e43fce",
+    "url": "/static/js/2.bb221f43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb221f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b60bff2f4a23814a52",
+    "url": "/static/js/main.d0674239.chunk.js"
+  },
+  {
+    "revision": "3025c8bddcf7af55362a",
+    "url": "/static/js/runtime-main.7c557974.js"
+  },
+  {
+    "revision": "154525edfaaf2abdf0400a0ef3a9a15a",
+    "url": "/static/media/Ainz Ooal Gown.154525ed.png"
+  },
+  {
+    "revision": "154525edfaaf2abdf0400a0ef3a9a15a",
+    "url": "/static/media/AinzMap.154525ed.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a94f72dc5397dd9f62c86ba6beaea298",
+    "url": "/static/media/tower basic.a94f72dc.png"
+  }
+]);
