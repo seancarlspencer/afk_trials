8b02de1fbd684b93fdc7849029900527cb94fc5a
seancarlspencer <47134137+seancarlspencer@users.noreply.github.com>
2020-11-03 00:20:43 -0500

    Changed file format from webp to jpg

--- src/components/Trial/Hero.js
+++ src/components/Trial/Hero.js
@@ -10,7 +10,7 @@ class Hero extends React.Component {
                     <div className="hero-icon-container">
                         <img className="hero-border" src={border}></img>
                         {/* {this.props.checked === 1 ? <div><img className="hero-check" src={check}></img></div> : <div></div>} */}
-                        <img className="hero-icon" src={`/assets/champs/${this.props.name}.webp`}></img>
+                        <img className="hero-icon" src={`/assets/champs/${this.props.name}.jpg`}></img>
                         {this.props.formation ? <div className="hero-position-formation">{this.props.number}</div>
                         : <div></div>
                         }
